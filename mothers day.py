83f2cbe94dbae2add4bac9a6c805c41eb6194075
BreadCrumbs65 <103623561+BreadCrumbs65@users.noreply.github.com>
2022-05-08 15:06:06 -0400

    Delete mothers day.py

--- mothers day.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def main():
-  st.set_page_config(page_title= "Happy Mothers Day")
-  st.header("Happy Mothers Day Joanne Francis")
-  st.write("Although we do not always see eye to eye we are will always be family \n and I will always love you.")
-  st.write('Love, Adam Francis')
-  st.image('mothers-day-mom-poems-son-daughter.jpg')
-main()
